9816599e08c70b0142ae743e9a6a8549a12f5321
Ryan Graham <ryangrahamwpb@gmail.com>
2020-01-27 13:08:52 -0500

    Amend problem four and finish five.

--- homework.js
+++ homework.js
@@ -16,6 +16,13 @@ function greeting(Chase) {
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not. An odd number is a
 // number which, when divided by 2, has a remainder of 1 or -1.
+function isOdd(value) {
+  if (value % 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not. An even number is a
