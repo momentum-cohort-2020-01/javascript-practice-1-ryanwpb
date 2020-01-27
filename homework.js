8aaecaeec5b97344fcdf1546508e697b1be444b9
Ryan Graham <ryangrahamwpb@gmail.com>
2020-01-27 13:17:13 -0500

    Amend problem five.

--- homework.js
+++ homework.js
@@ -16,8 +16,8 @@ function greeting(Chase) {
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not. An odd number is a
 // number which, when divided by 2, has a remainder of 1 or -1.
-function isOdd(value) {
-  if (value % 2) {
+function isOdd(num) {
+  if (num % 1 == 0) {
     return true;
   } else {
     return false;
@@ -27,9 +27,6 @@ function isOdd(value) {
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not. An even number is a
 // number which, when divided by 2, has a remainder of 0
-// 6. Create a function called fahrenheitToCelsius that takes a
-// Fahrenheit temperature as an argument and returns the
-// temperature in Celsius.
 function isEven(num) {
   if (num % 2 == 0) {
     return true;
@@ -37,6 +34,10 @@ function isEven(num) {
     return false;
   }
 }
+
+// 6. Create a function called fahrenheitToCelsius that takes a
+// Fahrenheit temperature as an argument and returns the
+// temperature in Celsius.
 
 // 6. Create a function called celsiusToFahrenheit that takes a
 // Celsius temperature as an argument and returns the
